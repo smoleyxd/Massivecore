8592cd477ddff627f6034dbcc1e1736c62bc67f4
Connor 'Birb' McCormick <Birb@birb.tech>
2020-11-22 01:02:31 +1300

    Forced Sync/Async events for Serialization
    To address weirdness with EventMassiveBasicPlayerAlterPlayer becoming Async

---
+++ src/com/massivecraft/massivecore/event/EventMassiveCoreSync.java
@@ -0,0 +1,13 @@
+package com.massivecraft.massivecore.event;
+
+public abstract class EventMassiveCoreSync extends EventMassiveCore
+{
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	public EventMassiveCoreSync()
+	{
+		super(false);
+	}
+}
