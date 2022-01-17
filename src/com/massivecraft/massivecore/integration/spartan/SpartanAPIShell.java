ff8337b34e2ef253e59173424f1b78a093b1174b
Connor 'Birb' McCormick <Birb@birb.tech>
2022-01-18 12:42:56 +1300

    Velocity Protection Fixes

--- src/com/massivecraft/massivecore/integration/spartan/SpartanAPIShell.java
+++ src/com/massivecraft/massivecore/integration/spartan/SpartanAPIShell.java
@@ -1,6 +1,7 @@
 package com.massivecraft.massivecore.integration.spartan;
 
 import me.vagdedes.spartan.api.API;
+import me.vagdedes.spartan.system.Enums.HackType;
 import org.bukkit.entity.Player;
 
 public class SpartanAPIShell
@@ -12,5 +13,8 @@ public class SpartanAPIShell
 	public static void disableVelocityProtection(Player p, int ticks)
 	{
 		API.disableVelocityProtection(p, ticks);
+		API.cancelCheck(p, HackType.Clip, ticks);
+		API.cancelCheck(p, HackType.IrregularMovements, ticks);
+		API.cancelCheck(p, HackType.Speed, ticks);
 	}
 }
